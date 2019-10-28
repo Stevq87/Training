79d5763dcb3fbd998038659a52b5245b8b8fce55
nismanVive <56160252+nismanVive@users.noreply.github.com>
2019-10-28 10:59:54 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,54 @@
+$('document').ready(function(){
+        //slider
+    $('.bxSlider').bxSlider({
+        mode:'fade',
+        caption: true,
+        slideWidth: 1200,
+        responsive: true,
+        slideMargin: 800,
+        pager: false
+    });
+    //post
+    var post= [
+        {
+            title: 'prueba de titulo 1',
+            date: 'Publicado el dia '+moment().format("DD")+' del mes de '+moment().format("MMMM")+' del año '+moment().format("YYYY"),
+            content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit Duis porta lectus enim,eu vestibulum nulla posuere vitae.Integer quis lacus et felis tempor hendrerit.Sed feugiat lectus id nibh fermentum lobortis.`
+        },
+        {
+            title: 'prueba de titulo 2',
+            date: 'Publicado el dia '+moment().format("DD")+' del mes de '+moment().format("MMMM")+' del año '+moment().format("YYYY"),
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Duis porta lectus enim,eu vestibulum nulla posuere vitae.Integer quis lacus et felis tempor hendrerit.Sed feugiat lectus id nibh fermentum lobortis."
+        },
+        {
+            title: 'prueba de titulo 3',
+            date: 'Publicado el dia '+moment().format("DD")+' del mes de '+moment().format("MMMM")+' del año '+moment().format("YYYY"),
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Duis porta lectus enim,eu vestibulum nulla posuere vitae.Integer quis lacus et felis tempor hendrerit.Sed feugiat lectus id nibh fermentum lobortis."
+        },
+        {
+            title: 'prueba de titulo 4',
+            date: 'Publicado el dia '+moment().format("DD")+' del mes de '+moment().format("MMMM")+' del año '+moment().format("YYYY"),
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Duis porta lectus enim,eu vestibulum nulla posuere vitae.Integer quis lacus et felis tempor hendrerit.Sed feugiat lectus id nibh fermentum lobortis."
+        },
+        {
+            title: 'prueba de titulo 4',
+            date: 'Publicado el dia '+moment().format("DD")+' del mes de '+moment().format("MMMM")+' del año '+moment().format("YYYY"),
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Duis porta lectus enim,eu vestibulum nulla posuere vitae.Integer quis lacus et felis tempor hendrerit.Sed feugiat lectus id nibh fermentum lobortis."
+        },
+    ]
+    post.forEach((item, index) =>{
+        var post= `
+        <article class="post">
+        <h2>${item.title}</h2>
+        <span class="date">${item.date}</span>
+        <p>
+            ${item.content}
+        </p>
+         <a href="#" class="button-more">Leer mas.</a>
+
+    </article>
+        `;
+        $('#posts').append(post);
+     });
+
+});
